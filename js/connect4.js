02a3ac917acd8d451e3036202561e53ec64d61d7
Dan Westaway <dan.westaway@students.plymouth.ac.uk>
2017-11-17 23:49:52 +0000

    Win checks
    Added some checks for a winner, not fully working yet

--- js/connect4.js
+++ js/connect4.js
@@ -35,15 +35,7 @@ $(function() { //jquery handler
             }
           }
         }
-        //Draw circle, parameters: x and y coordinates and colour
-        function createCircle(x, y, circleColour) {
 
-            context.beginPath();
-            //Draw circle, x and y are the middle coordinates for the circle, third parameter is radius
-            context.arc(x, y, 45, 0, 2 * Math.PI, false);
-            context.fillStyle = circleColour;
-            context.fill();
-        }
         //Calculate the row and column into exact coordinates on canvas
         function calculateCircle(col, row, name) {
           //cx = center x y coordinates calculated from column and row number
@@ -52,17 +44,29 @@ $(function() { //jquery handler
 
           if(name == "red")
           {
-            createCircle(centerX, centerY, circle.red.colour);
+              createCircle(centerX, centerY, circle.red.colour);
+
           }
           else if(name == "yellow")
           {
-            createCircle(centerX, centerY, circle.yellow.colour);
+              createCircle(centerX, centerY, circle.yellow.colour);
+
           }
           else
           {
-            createCircle(centerX, centerY, 'white');
+              createCircle(centerX, centerY, 'white');
           }
 
+        }
+
+        //Draw circle, parameters: x and y coordinates and colour
+        function createCircle(x, y, circleColour) {
+
+            context.beginPath();
+            //Draw circle, x and y are the middle coordinates for the circle, third parameter is radius
+            context.arc(x, y, 45, 0, 2 * Math.PI, false);
+            context.fillStyle = circleColour;
+            context.fill();
         }
 
         //Add circle colour into grid array
@@ -74,36 +78,94 @@ $(function() { //jquery handler
                 {
                     grid[row][column] = colour;
 
+                    checkForWin(row, column);
+
                     return true;
                 }
             }
             return false;
 
         }
-        
+
         var turn = 'red';
 
+        //Mouse click listener
         canvas.addEventListener('click', function(evt) {
 
+            text = document.getElementById('text');
+
+            //swap turns, change text and place circle on each click
             if(turn == 'red')
             {
                 drawCircle(getColumnClick(event), 'red');
                 turn = 'yellow';
+
+                text.innerHTML = "Yellow Player's Turn";
+                text.style.color = "yellow";
             }
             else
             {
                 drawCircle(getColumnClick(event), 'yellow');
                 turn = 'red';
+
+                text.innerHTML = "Red Player's Turn";
+                text.style.color = "red";
             }
 
             refreshGrid();
+
+            //console.log(grid);
         });
 
+        //Find out which column is clicked
         function getColumnClick(event) {
             var rect = canvas.getBoundingClientRect();
             var x = event.clientX - rect.left;
 
             return Math.floor(x / 100);
+        }
+
+        function checkForWin(row, col) {
+
+          //Check for 4 up
+          //check if higher than third row up or row index will be out of range
+          if(row > 2)
+          {
+            if(grid[row][col] == grid[row-1][col])
+            {
+              if(grid[row][col] == grid[row-2][col])
+              {
+                if(grid[row][col] == grid[row-3][col])
+                {
+                    alert("winner");
+                }
+              }
+            }
+          }
+
+          //Check for 4 to the right
+          if(grid[row][col] == grid[row][col+1])
+          {
+            if(grid[row][col] == grid[row][col+2])
+            {
+              if(grid[row][col] == grid[row][col+3])
+              {
+                  alert("winner");
+              }
+            }
+          }
+          //Check for 4 to the left
+          if(grid[row][col] == grid[row][col-1])
+          {
+            if(grid[row][col] == grid[row][col-2])
+            {
+              if(grid[row][col] == grid[row][col-3])
+              {
+                  alert("winner");
+              }
+            }
+          }
+
         }
 
 
