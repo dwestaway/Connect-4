4c965cd567fbf0f3dbb76e8ae66693b3860a1c40
Dan Westaway <dan.westaway@students.plymouth.ac.uk>
2017-12-15 17:42:35 +0000

    Node.js
    Node.js now loads html file on the localhost

--- js/server.js
+++ js/server.js
@@ -1,9 +1,19 @@
 var http = require('http');
+var fs = require('fs'); //fs = file system
 
 function onRequest(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello World!');
-    response.end();
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./index.html', null, function(error, data) {
+      //check if index.html exists
+      if(error) {
+        response.writeHead(404);
+        response.write('File not found');
+      }
+      else {
+        response.write(data);
+      }
+      response.end();
+    });
 }
 
 http.createServer(onRequest).listen(8000);
