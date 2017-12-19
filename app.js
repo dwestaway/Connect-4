4552c8ad8e43bdb80a7e526e86308900203e18c9
Dan Westaway <dan.westaway@students.plymouth.ac.uk>
2017-12-19 02:45:12 +0000

    Node.js
    Setup node.js, can now send messages forwards and back from server using socket.io

--- app.js
+++ app.js
@@ -8,3 +8,19 @@ app.get('/', function(req, res) {
 app.use('/client',express.static(__dirname + '/client'));
 
 serv.listen(2000);
+console.log("Server started.");
+
+
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function(socket){
+    console.log('Client connected through socket');
+
+    //recieve message
+    socket.on('hello',function() {
+        console.log('hello from client');
+    });
+    //send message
+    socket.emit('serverMsg',{
+        msg:'hello from server',
+    });
+});
